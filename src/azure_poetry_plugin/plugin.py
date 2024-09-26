b6a15625d05b7ed6606452cb0e6894899aafbdb1
Daniel Gottlieb Dollerup <25936896+dgot@users.noreply.github.com>
2024-09-26 17:53:12 +0200

    test in multiple python versions

--- src/azure_poetry_plugin/plugin.py
+++ src/azure_poetry_plugin/plugin.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable
+from typing import Callable, Union
 
 from cleo.commands.command import Command
 from cleo.helpers import argument, option
@@ -35,7 +35,7 @@ def set_access_token(
 def get_access_token(
     feed_name: str,
     config: Config,
-) -> dict[str, str | None] | None:
+) -> Union[dict[str, Union[str, None]], None]:
     """Get the access token configured with poetry for the artifact feed."""
     password_manager = PasswordManager(config=config)
     access_token = password_manager.get_http_auth(repo_name=feed_name)
