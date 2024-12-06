850d85c5faa075bfcd909e1c18115a5454cf2670
Daniel Gottlieb Dollerup <25936896+dgot@users.noreply.github.com>
2024-12-06 11:26:40 +0100

    create poetry config using internals

--- test/azure_poetry_plugin/test_plugin.py
+++ test/azure_poetry_plugin/test_plugin.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 
@@ -5,24 +6,34 @@ import pytest
 from cleo.application import Application
 from cleo.exceptions import CleoMissingArgumentsError
 from cleo.testers.command_tester import CommandTester
+from poetry.config.config import CONFIG_DIR, Config
 
 from azure_poetry_plugin.commands import (
     AddCommand,  # noqa: F401
     ConfigCommand,  # noqa: F401
 )
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.fixture(scope="session")
-def _poetry_config() -> Path:
+def poetry_config() -> Path:
     """Ensure a poetry configuration exists."""
-    import subprocess
-    subprocess.run(
-        ["poetry config repositories.baselifescience"],
-        check=True,
-    )
+    config = Config.create()
+    config_file = Path(os.path.join(CONFIG_DIR, "config.toml"))
+    if not config.config_source.file.exists():
+        os.makedirs(CONFIG_DIR, exist_ok=True)
+        try:
+            open(config_file, "x").close()
+        except FileExistsError:
+            logger.warning(
+                f"poetry configuration file at: {config_file} "
+                "was created by another process."
+            )
+    return config_file
 
 
-def test_add_command() -> None:
+def test_add_command(poetry_config: Path) -> None:
     """Execute and verify the add command."""
     application = Application()
     application.add(AddCommand())
@@ -34,7 +45,7 @@ def test_add_command() -> None:
     )
 
 
-def test_config_command() -> None:
+def test_config_command(poetry_config: Path) -> None:
     """Execute and verify the config command."""
     application = Application()
     application.add(ConfigCommand())
